a9e3206985237a8a3e455ed587f0e9f1d3854c1c
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-02 09:43:58 +0800

    Add comments

--- evenOdd/Program.cs
+++ evenOdd/Program.cs
@@ -4,9 +4,11 @@
     {
         int i;
 
+        // prompt user for integer
         Console.Write("Input a number: ");
         i = Convert.ToInt32(Console.ReadLine());
 
+        // check parity of integer
         if (i % 2 == 0)
         {
             Console.WriteLine("even");
