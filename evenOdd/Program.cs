b7227900dc43cab118475bd9e408f8cefceee4ee
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-02 09:36:16 +0800

    Add if else statements that determine even odd number

--- evenOdd/Program.cs
+++ evenOdd/Program.cs
@@ -6,5 +6,14 @@
 
         Console.Write("Input a number: ");
         i = Convert.ToInt32(Console.ReadLine());
+
+        if (i % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else
+        {
+            Console.WriteLine("odd");
+        }
     }
 }
